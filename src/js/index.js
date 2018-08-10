0e5fe1f895c54a6a7e1310955b7f3a80c1451c53
Moorthy <moorthy.g@krds.fr>
2018-08-10 12:28:18 +0530

    Include normalize.css

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import 'js/polyfills';
-import '../style/main.less';
 import 'js/app-module.js';
+import 'normalize.css';
+import '../style/main.less';
 
 if (module.hot) {
   module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const rules = [
     }
   },
   {
-    test: /\.less$/,
+    test: /\.(less|css)$/,
     use: [
         //minimize css in prod build to avoid bundling newline chars in js chunk
         { loader: isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader },
@@ -149,13 +149,13 @@ module.exports = {
       chunks: 'all',
       cacheGroups: {
         vendors: {
-          test: /[\\/]node_modules[\\/]/,
+          test: /[\\/]node_modules[\\/].+\.js$/,
           priority: -10,
           name: 'vendor'
         },
         styles: {
           name: 'main',
-          test: /\.less$/,
+          test: /\.(less|css)$/,
           chunks: 'all',
           enforce: true
         }
